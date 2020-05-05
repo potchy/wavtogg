6a6c56ea16d95d28e2aaba0a65f03535055c6324
potchy <potchy@protonmail.com>
2020-05-05 18:40:49 -0300

    First release

---
+++ wavtogg/ConvertOptions.cs
@@ -0,0 +1,68 @@
+﻿using CommandLine;
+
+namespace wavtogg
+{
+    [Verb("convert", HelpText = "Converts all .wav files in a folder to .ogg.")]
+    public class ConvertOptions
+    {
+        [Option(
+            "ffmpeg",
+            Required = true,
+            HelpText = @"Path to ffmpeg's executable. e.g: ""C:\ffmpeg\bin\ffmpeg.exe"""
+        )]
+        public string FfmpegPath { get; set; }
+
+        [Option(
+            'q',
+            "qscale",
+            Default = 5,
+            HelpText = "Audio quality. Range from -1 to 10."
+        )]
+        public int Quality { get; set; }
+
+        [Option(
+            'i',
+            "input",
+            Required = true,
+            HelpText = @"Path to the folder containing the .wav files. e.g: ""C:\bms\bofxv\song"""
+        )]
+        public string InputPath { get; set; }
+
+        [Option(
+            'r',
+            "recursive",
+            HelpText = "Allows traversing through the input folder recursively."
+        )]
+        public bool AllowRecursion { get; set; }
+
+        [Option(
+            'o',
+            "overwrite",
+            HelpText = "Allows overwrite, if .wav equivalents already exist in the input folder. Those cases are skipped by default."
+        )]
+        public bool AllowOverwrite { get; set; }
+
+        [Option(
+            'b',
+            "backup",
+            SetName = "backup",
+            Default = Program.DefaultBackupFolderName,
+            HelpText = "Backup folder name. Folders with this name are automatically excluded from the conversion process."
+        )]
+        public string BackupFolderName { get; set; }
+
+        [Option(
+            "no-backup",
+            SetName = "no-backup",
+            HelpText = "Disables backup."
+        )]
+        public bool DoNotBackup { get; set; }
+
+        [Option(
+            'l',
+            "log",
+            HelpText = "Path to the log file to be written to."
+        )]
+        public string LogFilePath { get; set; }
+    }
+}
---
+++ wavtogg/RevertOptions.cs
@@ -0,0 +1,52 @@
+﻿using CommandLine;
+
+namespace wavtogg
+{
+    [Verb("revert", HelpText = "Restore all backed up .wav files in a folder.")]
+    public class RevertOptions
+    {
+        [Option(
+            'i',
+            "input",
+            Required = true,
+            HelpText = @"Path to the folder containing the .ogg files. e.g: ""C:\bms\bofxv\song"""
+        )]
+        public string InputPath { get; set; }
+
+        [Option(
+            'r',
+            "recursive",
+            HelpText = "Allows traversing through the input folder recursively."
+        )]
+        public bool AllowRecursion { get; set; }
+
+        [Option(
+            'o',
+            "overwrite",
+            HelpText = "Allows overwrite, if a file with the same name (with extension) as the backup already exists in the input folder. Those cases are skipped by default."
+        )]
+        public bool AllowOverwrite { get; set; }
+
+        [Option(
+            'b',
+            "backup",
+            SetName = "backup",
+            Default = Program.DefaultBackupFolderName,
+            HelpText = "Backup folder name."
+        )]
+        public string BackupFolderName { get; set; }
+
+        [Option(
+            "preserve-ogg",
+            HelpText = "If specified, .ogg files with the same name (without extension) as the backed up .wav will not be deleted after the backup is restored."
+        )]
+        public bool KeepOggFile { get; set; }
+
+        [Option(
+            'l',
+            "log",
+            HelpText = "Path to the log file to be written to."
+        )]
+        public string LogFilePath { get; set; }
+    }
+}
